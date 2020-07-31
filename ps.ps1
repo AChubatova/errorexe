087de26893c248747313b3a662f984a74d714dc9
AChubatova <64786092+AChubatova@users.noreply.github.com>
2020-07-31 01:34:51 -0700

    Update ps.ps1

--- ps.ps1
+++ ps.ps1
@@ -1,7 +1,8 @@
-& ".\main.exe"
+#& ".\main.exe"
+& "C:\Program Files\mingw-w64\x86_64-8.1.0-posix-seh-rt_v6-rev0\mingw64\bin\g++.exe" mag.cpp
 #& minikube start --nfs-share="c:\dd"
 #& "C:\Program Files\JetBrains\IntelliJ IDEA 2020.1.3\bin\idea.exe" 
 if ($LastExitCode -ne 0) {
-    #Throw "Process exit code: $LastExitCode";
+    Throw "Process exit code: $LastExitCode";
     return 1}
 #Invoke-WebRequest "a"
