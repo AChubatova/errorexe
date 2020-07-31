023a5ee39905c579c89ece90f46c668c4c11ac52
AChubatova <64786092+AChubatova@users.noreply.github.com>
2020-07-31 04:10:52 -0700

    Update ps.ps1

--- ps.ps1
+++ ps.ps1
@@ -1,5 +1,5 @@
 #& ".\main.exe"
-& ".\g++.exe" mag.cpp
+& g++.exe main_error.cpp
 #& "C:\Program Files\mingw-w64\x86_64-8.1.0-posix-seh-rt_v6-rev0\mingw64\bin\g++.exe" mag.cpp
 #& minikube start --nfs-share="c:\dd"
 #& "C:\Program Files\JetBrains\IntelliJ IDEA 2020.1.3\bin\idea.exe" 
