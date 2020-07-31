ae2a306c90f010bf7cb54d03fda83ecec2f0c43c
AChubatova <64786092+AChubatova@users.noreply.github.com>
2020-07-31 01:30:28 -0700

    Update ps.ps1

--- ps.ps1
+++ ps.ps1
@@ -2,5 +2,6 @@
 #& minikube start --nfs-share="c:\dd"
 #& "C:\Program Files\JetBrains\IntelliJ IDEA 2020.1.3\bin\idea.exe" 
 if ($LastExitCode -ne 0) {
-    Throw "Process exit code: $LastExitCode";}
+    Throw "Process exit code: $LastExitCode";
+    return 1}
 #Invoke-WebRequest "a"
