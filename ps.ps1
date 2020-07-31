c869a157e957c1c6208383e6eec4008d1a4fa2e9
AChubatova <64786092+AChubatova@users.noreply.github.com>
2020-07-31 01:24:02 -0700

    Update ps.ps1

--- ps.ps1
+++ ps.ps1
@@ -1,5 +1,5 @@
-#& ".\errorexe.exe"
-& minikube start --nfs-share="c:\dd"
+& ".\main.exe"
+#& minikube start --nfs-share="c:\dd"
 #& "C:\Program Files\JetBrains\IntelliJ IDEA 2020.1.3\bin\idea.exe" 
 if ($LastExitCode -ne 0) {
     Throw "Process exit code: $LastExitCode";}
