a07a0af4b38bd79664bdf28825b989a31e33d232
AChubatova <64786092+AChubatova@users.noreply.github.com>
2020-07-31 04:09:23 -0700

    Add files via upload

---
+++ main_error.cpp
@@ -0,0 +1,20 @@
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    std::cerr << "error string 1" << std::endl;
+	std::cerr << "error string 2" << std::endl;
+	std::cerr << "error string 1" << std::end;
+	kjk
+    //fstream myfile;
+    //myfile.open("rofile1.txt", ios::out);
+   
+    //myfile << "Writing this to a file.\n";
+    return 1;
+}
